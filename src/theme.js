1cc17dac24e2b75c25c3a7ddea511900471ea5a9
laindomJS <adrijar21@gmail.com>
2023-06-01 23:12:38 -0400

    initial commmit - setup project

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+/* eslint-disable quotes */
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    primary: {
+      'custom-tomato': 'hsl(4, 100%, 67%)'
+    },
+    neutral: {
+      'dark-slate-grey': 'hsl(234, 29%, 20%)',
+      'charcoal-gray': 'hsl(235, 18%, 26%)',
+      'custom-grey': 'hsl(231, 7%, 60%)'
+    }
+  },
+  fonts: {
+    headings: `'Roboto', sans-serif`,
+    body: `'Roboto', sans-serif`
+  }
+})
